c1d91137a3e64c04fa6e8b3edc1a47f16954a636
Abdullah <iabdullahkhan438@gmail.com>
2023-05-26 15:29:29 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python GUI project
+
+Project is for demo of GUI in Python
+
+>Tools: Tkinter, poetry, python
---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+def print_hi(good_name):
+    print(f'Hi, {good_name}')
+
+
+if __name__ == '__main__':
+    name = input("Please enter your name: ")
+    print_hi(f'{name}')
